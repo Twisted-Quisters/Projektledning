7a9737c545d532118bddba58f3d09c1dd5e87e03
clarasundstrom <56594158+clarasundstrom@users.noreply.github.com>
2020-03-06 13:42:00 +0100

    updated class
    Skadeanmälan

--- src/application/Skadeanmälan.java
+++ src/application/Skadeanmälan.java
@@ -1,5 +1,14 @@
 package application;
 
 public class Skadeanmälan {
- //test
+ private int skadeId;
+ private String skadeTyp;
+ private String status;
+ private int prioritering;
+ private String åtgärd;
+ private String tid;
+ private String plats;
+ 
+ 
+ 
 }
