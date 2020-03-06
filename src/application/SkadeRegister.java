06090e4b747e95fece6d093b708dbf08cebdaf98
olivianmhill <ol5382hi-s@student.lu.se>
2020-03-06 14:51:17 +0100

    damage id generator

--- src/application/SkadeRegister.java
+++ src/application/SkadeRegister.java
@@ -5,15 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SkadeRegister {
-<<<<<<< HEAD
-private HashMap <Integer, Skadeanmälan> DamageList = new HashMap <Integer, Skadeanmälan>(); 
-=======
 	private HashMap <Integer, Skadeanmälan> damageList = new HashMap <Integer, Skadeanmälan>();
 	private ArrayList<String> incoming = new ArrayList<String>();
 	private ArrayList<String> ongoing = new ArrayList<String>();
 	private ArrayList<String> finished = new ArrayList<String>();
 	private ArrayList<String> forwarded = new ArrayList<String>();
->>>>>>> Olivia
+
 
 	public HashMap <Integer, Skadeanmälan> getSkadeLista() {
 		return damageList;
@@ -50,10 +47,15 @@ private HashMap <Integer, Skadeanmälan> DamageList = new HashMap <Integer, Skad
 	public void setForwarded(ArrayList<String> forwarded) {
 		this.forwarded = forwarded;
 	}
+	
+	public void addDamage(Skadeanmälan skada) {
+		damageList.put(skada.getDamageId(), skada);
+	}
+	
 	// inkommande ärenden
 	public ArrayList<String> showIncoming() {
 		for(Map.Entry<Integer, Skadeanmälan> skada : damageList.entrySet()) {
-			if(skada.getValue().getStatus.equals("Inkommande")) {
+			if(skada.getValue().getStatus().equals("Inkommande")) {
 				incoming.add(skada.getKey() + skada.getValue().getTitle());
 			}
 			
@@ -64,7 +66,7 @@ private HashMap <Integer, Skadeanmälan> DamageList = new HashMap <Integer, Skad
 	//pågående ärenden
 	public ArrayList<String> showOngoing() {
 		for(Map.Entry<Integer, Skadeanmälan> skada : damageList.entrySet()) {
-			if(skada.getValue().getStatus.equals("Pågående")) {
+			if(skada.getValue().getStatus().equals("Pågående")) {
 				ongoing.add(skada.getKey() + skada.getValue().getTitle());
 			}
 			
@@ -75,7 +77,7 @@ private HashMap <Integer, Skadeanmälan> DamageList = new HashMap <Integer, Skad
 	//avslutade ärenden
 	public ArrayList<String> showFinished() {
 		for(Map.Entry<Integer, Skadeanmälan> skada : damageList.entrySet()) {
-			if(skada.getValue().getStatus.equals("Avslutad")) {
+			if(skada.getValue().getStatus().equals("Avslutad")) {
 				finished.add(skada.getKey() + skada.getValue().getTitle());
 			}
 			
@@ -85,7 +87,7 @@ private HashMap <Integer, Skadeanmälan> DamageList = new HashMap <Integer, Skad
 	}
 	public ArrayList<String> showForwarded() {
 		for(Map.Entry<Integer, Skadeanmälan> skada : damageList.entrySet()) {
-			if(skada.getValue().getStatus.equals("Pågående")) {
+			if(skada.getValue().getStatus().equals("Pågående")) {
 				forwarded.add(skada.getKey() + skada.getValue().getTitle());
 			}
 			
