3c9f769d32647f1126b907f8f1e3b8f5741707b7
clarasundstrom <56594158+clarasundstrom@users.noreply.github.com>
2020-03-06 14:13:27 +0100

    setters and getters
    and changed from swedish to english

--- src/application/Skadeanmälan.java
+++ src/application/Skadeanmälan.java
@@ -1,14 +1,88 @@
 package application;
 
 public class Skadeanmälan {
- private int skadeId;
- private String skadeTyp;
+ private int damageId;
+ private String damageType;
  private String status;
- private int prioritering;
- private String åtgärd;
- private String tid;
- private String plats;
+ private int priority;
+ private String action;
+ private String time;
+ private String place;
+ private String title;
+ private String comment;
  
+ public int getDamageId() {
+	 return damageId;
+ }
  
+ public void setDamageId(int newDamageId) {
+		damageId = newDamageId;
+ }
+ 
+ public String getDamageType() {
+	 return damageType;
+ }
+ 
+ public void setDamageType(String newDamageType) {
+		damageType = newDamageType; 
+ }
+ 
+ public String getStatus() {
+	 return status;
+ }
+ 
+ public void setStatus (String newStatus) {
+		status = newStatus;
+}
+ 
+ public int getPriority () {
+	 return priority;
+ }
+ 
+ public void setPriority(int newPriority) {
+		priority = newPriority;
+ }
+ 
+ public String getAction () {
+	 return action;
+ }
+ 
+ public void setAction(String newAction) {
+		action = newAction;
+ }
+ public String getTime() {
+	 return time;
+ }
+ 
+ public void setTime(String newTime) {
+		time = newTime;
+ }
+ 
+ public String getPlace() {
+	 return place;
+ }
+ 
+ public void setPlace(String newPlace) {
+		place = newPlace;
+ }
+ 
+ public String getTitle() {
+	 return title;
+ }
+ 
+ public void setTitle(String newTitle) {
+		title = newTitle;
+ }
+ 
+ public String getComment() {
+	 return comment;
+ }
+ 
+ public void setComment(String newComment) {
+		comment = newComment;
+ }
  
 }
+
+ 
+ 
